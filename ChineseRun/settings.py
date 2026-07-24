a949c9bc7411850eb36aed0f0fd2d5e545ba5f91
Cece Moo <cecemoo@Ceces-MacBook-Air.local>
2026-07-24 16:39:54 -0500

    Fix ALLOWED_HOSTS environment parsing

--- ChineseRun/settings.py
+++ ChineseRun/settings.py
@@ -19,10 +19,14 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv(
-    "DJANGO_ALLOWED_HOSTS",
-    "LOCALHOST, 127.0.0.1",
-).split(", ")
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.getenv(
+        "DJANGO_ALLOWED_HOSTS",
+        "localhost,127.0.0.1"
+    ).split(",")
+    if host.strip()
+]
 
 
 # Application definition
