1ac04f6aaaec468dd00621a94e51348184118da0
Cece Moo <cecemoo@Ceces-MacBook-Air.local>
2026-08-19 10:39:29 -0500

    Implement reset password functionality

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from . import managers
-
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -9,6 +9,16 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('my_login/', views.my_login, name='my_login'),
     path('user_logout/', views.user_logout, name='user_logout'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/password_reset.html',
+        email_template_name='account/password_reset_email.html',
+        subject_template_name='account/password_reset_subject.txt'
+    ), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'), name='password_reset_complete'),
+
 
     path('faq/', views.faq, name='faq'),
     path('terms/', views.terms, name='terms'),
