7e9bcd322c6db594d087c878f175af547224f9b8
cecemoo <142958055+cecemoo@users.noreply.github.com>
2026-09-09 19:38:35 -0500

    Add tutoring dispute route

--- course/urls.py
+++ course/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('booking/<int:pk>/session/join/', session_views.session_join, name='session_join'),
     path('booking/<int:pk>/session/heartbeat/', session_views.session_heartbeat, name='session_heartbeat'),
     path('booking/<int:pk>/session/leave/', session_views.session_leave, name='session_leave'),
+    path('booking/<int:pk>/session/report/', session_views.report_session_issue, name='report_session_issue'),
 
     path("set-timezone/", views.set_student_timezone, name="set_student_timezone"),
 
