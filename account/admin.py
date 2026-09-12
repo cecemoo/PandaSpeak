e96442831696baf09fa73d06099685e86b55ba5a
cecemoo <142958055+cecemoo@users.noreply.github.com>
2026-09-12 16:39:34 -0500

    Add announcements to Django admin

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
-from . models import CustomUser, TermsOfService, PrivacyPolicy, PlacementQuestion
-# Register your models here.
+from .models import (
+    Announcement,
+    AnnouncementDelivery,
+    CustomUser,
+    PlacementQuestion,
+    PrivacyPolicy,
+    TermsOfService,
+)
+
+
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'created_by', 'created_at', 'queued_at', 'delivery_count')
+    list_filter = ('created_at', 'queued_at')
+    search_fields = ('subject', 'body', 'created_by__email')
+    readonly_fields = ('created_at', 'queued_at')
+    ordering = ('-created_at',)
+
+    @admin.display(description='Deliveries')
+    def delivery_count(self, obj):
+        return obj.deliveries.count()
+
+
+@admin.register(AnnouncementDelivery)
+class AnnouncementDeliveryAdmin(admin.ModelAdmin):
+    list_display = ('announcement', 'user', 'status')
+    list_filter = ('status',)
+    search_fields = ('announcement__subject', 'user__email')
+    autocomplete_fields = ('announcement', 'user')
 
 
 admin.site.register(CustomUser)
