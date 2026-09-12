6b43562999564117bd09e8b2e16ed2302b9d3558
cecemoo <142958055+cecemoo@users.noreply.github.com>
2026-09-12 11:03:36 -0500

    Register tutoring reschedule model

--- course/apps.py
+++ course/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CourseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'course'
+
+    def ready(self):
+        # RescheduleRequest lives in a separate module to keep tutoring
+        # rescheduling logic isolated while still registering the model.
+        from . import reschedule_models  # noqa: F401
