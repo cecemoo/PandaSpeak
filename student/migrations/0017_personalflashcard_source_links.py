c3edf171327d83a6e26137e70028bd6eb9ffce42
cecemoo <142958055+cecemoo@users.noreply.github.com>
2026-09-12 19:37:27 -0500

    Add source links to personal flashcards

---
+++ student/migrations/0017_personalflashcard_source_links.py
@@ -0,0 +1,46 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('student', '0016_personalflashcard'),
+        ('teacher', '0032_bingogame_expand_category_key'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='personalflashcard',
+            name='source_idiom',
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name='personal_flashcards',
+                to='teacher.idiom',
+            ),
+        ),
+        migrations.AddField(
+            model_name='personalflashcard',
+            name='source_sentence',
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name='personal_flashcards',
+                to='teacher.sentence',
+            ),
+        ),
+        migrations.AddField(
+            model_name='personalflashcard',
+            name='source_vocabulary',
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name='personal_flashcards',
+                to='teacher.vocabulary',
+            ),
+        ),
+    ]
