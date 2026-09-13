fa058068e7a8349070c7532bd31d9b8dd9926149
cecemoo <142958055+cecemoo@users.noreply.github.com>
2026-09-13 10:32:01 -0500

    Add Stripe subscription success route

--- subscription/urls.py
+++ subscription/urls.py
@@ -6,6 +6,7 @@ from . import stripe_subscription_views
 urlpatterns = [
     path('subscribe/', views.subscribe, name='subscribe'),
     path('stripe/checkout/', stripe_subscription_views.stripe_subscription_checkout, name='stripe_subscription_checkout'),
+    path('stripe/success/', stripe_subscription_views.stripe_subscription_success, name='stripe_subscription_success'),
     path('stripe/webhook/', stripe_subscription_views.stripe_subscription_webhook, name='stripe_subscription_webhook'),
     path('checkout/<int:payment_id>/', views.create_tutoring_checkout_session, name='create_tutoring_checkout_session'),
     path('success/', views.subscription_success, name='subscription_success'),
