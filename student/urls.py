440810aea86d2a55958d7033a661af37f3c58a69
cecemoo <142958055+cecemoo@users.noreply.github.com>
2026-09-12 20:17:22 -0500

    Restore existing student subscription views

--- student/urls.py
+++ student/urls.py
@@ -2,15 +2,14 @@ from django.urls import path
 from . import views
 from . import bingo_views
 from . import flashcard_views
-from . import subscription_views
 
 
 urlpatterns = [
     path('student_dashboard/', views.student_dashboard, name='student_dashboard'),
     path('access_learning_materials/', views.access_learning_materials, name='access_learning_materials'),
     path('learning_material_search/', views.learning_material_search, name='learning_material_search'),
-    path('subscription_plans/', subscription_views.subscription_plans, name='subscription_plans'),
-    path('account_management_student/', subscription_views.account_management, name='account_management_student'), 
+    path('subscription_plans/', views.subscription_plans, name='subscription_plans'),
+    path('account_management_student/', views.account_management, name='account_management_student'), 
     path('subscription_locked/', views.subscription_locked, name='subscription_locked'),  
 
     path('vocabularies/category/<int:category_id>/', views.vocabulary_category_page, name='vocabulary_category_page'),
