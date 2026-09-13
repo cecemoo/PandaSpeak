34e0de86599159683ee8179119b885162fa91cf7
cecemoo <142958055+cecemoo@users.noreply.github.com>
2026-09-12 20:05:22 -0500

    Remove duplicate subscription checkout URL

--- subscription/urls.py
+++ subscription/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
-    path('subscribe/', views.subscribe, name='subscribe'),
     path('checkout/<int:payment_id>/', views.create_tutoring_checkout_session, name='create_tutoring_checkout_session'),
     path('success/', views.subscription_success, name='subscription_success'),
     path('cancel/', views.cancel_subscription, name='cancel_subscription'),
